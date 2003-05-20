370c7cfa4be0412f0f9a36602a0c42db10b4ff23
Stephen Searle <searle@sanger.ac.uk>
2003-05-20 16:43:45 +0000

    Added to EcoStringTest

--- src/Tests/EcoStringTest.c
+++ src/Tests/EcoStringTest.c
@@ -4,26 +4,40 @@
 
 #include "BaseTest.h"
 
-#define TESTSTR1 "TestStr"
+char *TestStr1 = "TestStr";
 
 int main(int argc, char **argv) {
   ECOSTRTABLE *table;
   ECOSTRING    ecoS1;
   ECOSTRING    ecoS2;
+  ECOSTRING    ecoS3;
 
   if (!EcoString_initTable(&table)) {
     failAndDie("Couldn't create table\n");
   }
   ok(1, table != NULL);
 
-  EcoString_copyStr(table,&ecoS1, TESTSTR1, 0);
+  EcoString_copyStr(table,&ecoS1, TestStr1, 0);
 
   ok(2, ecoS1!=NULL);
-  ok(3, !strcmp(ecoS1,TESTSTR1));
+  ok(3, !strcmp(ecoS1,TestStr1));
   
-  EcoString_copyStr(table,&ecoS2, TESTSTR1, 0);
+  EcoString_copyStr(table,&ecoS2, TestStr1, 0);
   ok(4, ecoS1 == ecoS2);
   ok(5, !EcoString_strcmp(ecoS1,ecoS2));
+
+  EcoString_freeStr(table,ecoS1);
+  EcoString_freeStr(table,ecoS2);
+
+  EcoString_copyStr(table,&ecoS3, &(TestStr1[1]), 0);
+
+  EcoString_copyStr(table,&ecoS1, TestStr1, 0);
+
+/* Should have been reallocated */
+  ok(6, EcoString_strcmp(ecoS1,ecoS2));
+
+  ok(7, !strcmp(ecoS3,&(TestStr1[1])));
+
    
 }
 
