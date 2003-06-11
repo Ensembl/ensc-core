d4f695512a7525dfef56797405de94297fe3c748
Stephen Searle <searle@sanger.ac.uk>
2003-06-11 17:23:07 +0000

    Start of test for Slice and its adaptor

---
+++ src/Tests/SliceAdaptorTest.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include "SliceAdaptor.h"
+#include "DBAdaptor.h"
+#include "EnsC.h"
+
+#include "BaseTest.h"
+
+int main(int argc, char *argv[]) {
+  DBAdaptor *dba;
+  Slice *slice;
+  SliceAdaptor *sa;
+
+  initEnsC();
+
+  dba = DBAdaptor_new("kaka.sanger.ac.uk","anonymous",NULL,"homo_sapiens_core_12_31",3306,NULL);
+
+  ok(1,!strcmp("NCBI31",DBAdaptor_getAssemblyType(dba)));
+
+  sa = DBAdaptor_getSliceAdaptor(dba);
+
+  ok(2, sa!=NULL);
+
+  slice = SliceAdaptor_fetchByChrStartEnd(sa,"1",1,10000);
+
+  ok(3, slice!=NULL);
+
+  ok(4,!strcmp(Slice_getName(slice),"1:1-100000"));
+
+  printf("slice seq = %s\n",Slice_getSeq(slice));
+
+
+  return 0;
+}
