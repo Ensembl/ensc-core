8677be0c46aa65dc86e9243e423654f67e3ceb7a
Stephen Searle <searle@sanger.ac.uk>
2013-04-16 07:07:59 +0000

    Initial translation of RangeRegistry - still a bit of work to do on it

---
+++ src/DataModel/Mapper/RangeRegistry.c
@@ -0,0 +1,336 @@
+/*
+=head2 new
+
+  Arg [1]    : none
+  Example    : my $rr = Bio::EnsEMBL::Mapper::RangeRegistry->new();
+  Description: Creates a new RangeRegistry object
+  Returntype : Bio::EnsEMBL::Mapper::RangeRegistry
+  Exceptions : none
+  Caller     : AssemblyMapperAdaptor
+  Status     : Stable
+
+=cut
+*/
+
+RangeRegistry *RangeRegistry_new() {
+  RangeRegistry *registry;
+
+  if ((registry = (RangeRegistry *)calloc(1,sizeof(RangeRegistry))) == NULL) {
+    fprintf(stderr,"ERROR: Failed allocating space for registry\n");
+    exit(1);
+  }
+
+  registry->registry = IDHash_new(IDHASH_MEDIUM);
+
+  return registry;
+}
+
+void *RangeRegistry_flush(RangeRegistry *registry) {
+  IDHash_free(registry->registry, Vector_free);
+
+  registry->registry = IDHash_new(IDHASH_MEDIUM);
+}
+
+/*
+=head2 check_and_register
+
+  Arg [1]    : string $id
+               The id of the range to be checked/registered (e.g. a sequenceid)
+  Arg [2]    : int $start
+               The start of the range to be checked
+  Arg [3]    : int $end
+               The end of the range to be checked
+  Arg [4]    : (optional) int $rstart
+               The start of the range to be registered
+               if the checked range was not fully registered
+  Arg [5]    : (optional) int $rend
+               The end of the range to be registerd
+               if the checked range was not fully registered
+  Example    : $ranges=$rr->check_and_register('X',500,600, 1,1_000_000));
+  Description: Checks the range registry to see if the entire range
+               denoted by ($id : $start-$end) is already registered.  If
+               it already is, then undef is returned.  If it is not then
+               the range specified by $rstart and $rend is registered, and
+               a list of regions that were required to completely register
+               $rstart-$rend is returned.  If $rstart and $rend are not
+               defined they default to $start and $end respectively.
+
+               The reason there is a single call to do both the checking and
+               registering is to reduce the overhead. Much of the work to
+               check if a range is registered is the same as registering a
+               region around that range.
+  Returntype : undef or listref of [start,end] range pairs
+  Exceptions : throw if rstart is greater than start
+               throw if rend is less than end
+               throw if end is less than start
+               throw if id, start, or end are not defined
+  Caller     : AssemblyMapperAdaptor
+  Status     : Stable
+
+=cut
+*/
+
+// Perl version allows rStart and rEnd to be optional. I require them to be set - if you don't need them to be different to start and end,
+// just use the same values as start and end for rStart and rEnd
+
+// Also added a flag to indicate we actually want the gaps vector returned - quite often its not used in the caller and so would leak
+// memory
+Vector *RangeRegistry_checkAndRegister(RangeRegistry *reg, IDType id, long start, long end, long rStart, long rEnd, int wantGaps) {
+
+  // The following was commented out due to Ensembl Genomes requirements
+  // for bacterial genomes.
+  // The following was uncommented because I'm not caring about those requirements
+  if ( start > end ) {
+    fprintf(stderr, "start argument [%ld] must be less than (or equal to) end argument [%ld]\n", start, end);
+    exit(1);
+  }
+  
+  if ( rStart > rEnd ) {
+    fprintf(stderr, "rStart argument [%ld] must be less than (or equal to) rEnd argument [%ld]\n", rStart, rEnd);
+    exit(1);
+  }
+
+  if ( rStart > start ) {
+    fprintf(stderr, "rStart argument [%ld] must be less than (or equal to) start [%ld]\n", rStart, start);
+    exit(1);
+  }
+
+  if ( rEnd < end ) {
+    fprintf(stderr, "rEnd argument [%ld] must be greater than (or equal to) end [%ld]\n", rEnd, end);
+    exit(1);
+  }
+
+  IDHash *reg = RangeRegistry_getRegistry(registry);
+  Vector *list;
+  if (IDHash_contains(id)) {
+    list = IDHash_getValue(id);
+  } else {
+    list = Vector_new();
+    IDHash_add(reg, id, list);
+  }
+
+  
+  Vector *gapPairs = NULL;
+  if (wantGaps) {
+    gapPairs = Vector_new();
+  }
+
+  int len = Vector_getNumElement(list);
+
+  if (len == 0) {
+    //this is the first request for this id, return a gap pair for the
+    // entire range and register it as seen
+    CoordPair *cp = CoordPair_new(rStart, rEnd);
+    Vector_addElement(list, cp);
+
+    return list;
+  }
+
+  //####
+  // loop through the list of existing ranges recording any "gaps" where
+  // the existing range does not cover part of the requested range
+  // 
+
+  int startIdx = 0;
+  int endIdx   = Vector_getNumElement(list)-1;
+  int midIdx;
+  Range *range;
+
+  // binary search the relevant pairs
+  // helps if the list is big
+  while ( ( endIdx - startIdx ) > 1 ) {
+    midIdx = ( startIdx + endIdx ) >> 1;
+    range  = Vector_getElementAt(list, midIdx);
+
+    if ( Range_getEnd(range) < rStart ) {
+      startIdx = midIdx;
+    } else {
+      endIdx = midIdx;
+    }
+  }
+
+  long gapStart;
+  long gapEnd;
+  int rIdx = -1;
+  int rStartIdx = -1;
+  int rEndIdx;
+
+  gapStart = rStart;
+
+  int i;
+  for (i=startIdx; i < len ; i++ ) {
+    Range *pRange = Vector_getElementAt(list, i);
+    long pStart = Range_getStart(pRange);
+    long pEnd   = Range_getEnd(pRange);
+    
+    // no work needs to be done at all if we find a range pair that
+    // entirely overlaps the requested region
+    if ( pStart <= start && pEnd >= end ) {
+      return NULL;
+    }
+
+    // find adjacent or overlapping regions already registered
+    if ( pEnd >= ( rStart - 1 ) && pStart <= ( rEnd + 1 ) ) {
+      if ( rStartIdx < 0 ) { // Not yet been set
+        rStartIdx = i;
+      }
+      rEndIdx = i;
+    }
+
+    if ( pStart > rStart ) {
+      gapEnd = ( rEnd < pStart ) ? rEnd : pStart - 1;
+      if (wantGaps) {
+        CoordPair *cp = CoordPair_new(gapStart, gapEnd);
+        Vector_addElement(gapPairs, cp);
+      }
+    }
+
+    gapStart = ( rStart > pEnd ) ? rStart : pEnd + 1;
+
+    if ( pEnd >= rEnd && rIdx < 0 ) {
+      rIdx = i;
+      break;
+    }
+  }
+
+  // do we have to make another gap?
+  if ( gapStart <= rEnd ) {
+    if (wantGaps) {
+      CoordPair *cp = CoordPair_new(gapStart, rEnd);
+      Vector_addElement(gapPairs, cp);
+    }
+  }
+
+  // 
+  // Merge the new range into the registered list
+  // 
+  if (rStartIdx >= 0 ) { // rStartIdx has been set to something 
+    long newStart;
+    long newEnd;
+    Range *rStartIdxRange = Vector_getElementAt(list, rStartIdx); 
+    Range *rEndIdxRange   = Vector_getElementAt(list, rEndIdx); 
+
+    if ( rStart < Range_getStart(rStartIdxRange)) {
+      newStart = rStart;
+    } else {
+      newStart = Range_getStart(rStartIdxRange);
+    }
+
+    if ( rEnd > Range_getEnd(rEndIdxRange)) {
+      newEnd = rEnd;
+    } else {
+      newEnd = Range_getEnd(rEndIdxRange);
+    }
+
+    CoordPair *cp = CoordPair_new(newStart, newEnd);
+
+    // Think its <=
+    for (i=rStartIdx; i<=rEndIdx; i++) {
+      Vector_removeElementAt(list, rStartIdx); // Always remove from rStartIdx as array is shrinking by one each time called
+    }
+    Vector_insertElementAt(list, rStartIdx, cp);
+    //splice( @$list, $rstart_idx,
+    //        $rend_idx - $rstart_idx + 1,
+    //        [ $new_start, $new_end ] );
+
+  } else if (rIdx >= 0) {
+    CoordPair *cp = CoordPair_new(rStart, rEnd);
+    Vector_insertElementAt(list, rIdx, cp);
+    //splice( @$list, $r_idx, 0, [ $rstart, $rend ] );
+  } else {
+    CoordPair *cp = CoordPair_new(rStart, rEnd);
+    Vector_addElement(list, cp);
+  }
+
+  // Note if wantGaps is not set then gapPairs will be NULL - but you said you didn't want it so that should be OK
+  return gapPairs;
+}
+
+// overlap size is just added to make RangeRegistry generally more useful
+
+/*
+=head2 overlap_size
+
+  Arg [1]    : string $id
+  Arg [2]    : int $start
+  Arg [3]    : int $end
+  Example    : my $overlap_size = $rr->( "chr1", 1, 100_000_000 )
+  Description: finds out how many bases of the given range are registered
+  Returntype : int
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
+*/
+
+long RangeRegistry_overlapSize(RangeRegistry *registry, IDType id, long start, long end)  {
+  long overlap = 0;
+
+  if ( start > end ) return 0;
+
+  IDHash *reg = RangeRegistry_getRegistry(registry);
+  Vector *list;
+  if (IDHash_contains(id)) {
+    list = IDHash_getValue(id);
+  } else {
+    return 0; // No list for this id, so can't be any overlap 
+  }
+
+  int len = Vector_getNumElement(list);
+
+  if ( len == 0 ) {
+    fprintf(stderr, "Odd have zero length list in RangeRegistry_overlapSize\n");
+    return 0;
+  }
+
+  int startIdx = 0;
+  int endIdx   = Vector_getNumElement(list)-1;
+  int midIdx;
+  Range *range;
+
+  // binary search the relevant pairs
+  // helps if the list is big
+  while ( ( endIdx - startIdx ) > 1 ) {
+    midIdx = ( startIdx + endIdx ) >> 1;
+    range   = Vector_getElementAt(list, midIdx);
+    if ( Range_getEnd(range) < start ) {
+      startIdx = midIdx;
+    } else {
+      endIdx = midIdx;
+    }
+  }
+
+  int i;
+  for (i=startIdx; i < len ; i++ ) {
+    Range *pRange = Vector_getElementAt(list, i);
+    long pStart = Range_getStart(pRange);
+    long pEnd   = Range_getEnd(pRange);
+
+    if ( pStart > end ) {
+      break;
+    }
+
+    if ( pStart <= start && pEnd >= end ) {
+      overlap = end - start + 1;
+      break;
+    }
+
+    long mStart = ( start < pStart ? pStart : start );
+    long mEnd   = ( end   < pEnd   ? end    : pEnd );
+
+    if (mEnd - mStart >= 0) {
+      overlap += ( mEnd - mStart + 1 );
+    }
+  }
+
+  return overlap;
+}
+
+
+// low level function to access the ranges
+// only use for read access
+Vector *RangeRegistry_getRanges(RangeRegistry *registry, IDType id) {
+
+  return $self->{'registry'}->{$id};
+}
