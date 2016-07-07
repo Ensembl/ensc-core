0ed811e98cd60ca3c39d408cedf5909b1bcc3d46
Thibaut Hourlier <thibaut@ebi.ac.uk>
2016-07-07 17:18:49 +0100

    Copyright and formatting

--- src/Programs/bamhelper.h
+++ src/Programs/bamhelper.h
@@ -1,20 +1,37 @@
+/*
+ * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * Copyright [2016] EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "sam.h"
 #define MY_FUSEDFLAG 32768
 /*
  print out a bam1_t entry, particularly the flags (for debugging)
  /nfs/ensembl/searle/progs/production_code/ensembl-trunk_0713/samtools_parallel
 */
-void printBam(FILE *fp, bam1_t *b, bam_hdr_t *header) {    
+void printBam(FILE *fp, bam1_t *b, bam_hdr_t *header) {
   fprintf(fp, "%s %s %d %d %s (%d) %d %d %d\t\tP %d PP %d U %d MU %d R %d MR %d R1 %d R2 %d S %d QC %d D %d U %d\n",
-                                  bam_get_qname(b), 
-                                  header->target_name[b->core.tid], 
-                                  b->core.pos, 
+                                  bam_get_qname(b),
+                                  header->target_name[b->core.tid],
+                                  b->core.pos,
                                   //bam_calend(&b->core,bam1_cigar(b)),
                                   bam_endpos(b),
-                                  header->target_name[b->core.mtid], 
-                                  b->core.mtid, 
-                                  b->core.mpos, 
-                                  b->core.isize, 
+                                  header->target_name[b->core.mtid],
+                                  b->core.mtid,
+                                  b->core.mpos,
+                                  b->core.isize,
                                   bam_cigar2qlen(b->core.n_cigar, bam_get_cigar(b)),
                                   b->core.flag & BAM_FPAIRED,
                                   b->core.flag & BAM_FPROPER_PAIR ? 1 : 0,
