1cfee4411c1030e59c58f214b9ff6b4af92c6c7a
Thibaut Hourlier <thibaut@ebi.ac.uk>
2018-10-08 19:56:05 +0100

    Use DBAdaptorTest to check to check if the API works properly

--- src/Tests/BaseRODBTest.h
+++ src/Tests/BaseRODBTest.h
@@ -25,16 +25,7 @@
 DBAdaptor *Test_initROEnsDB() {
   DBAdaptor *dba;
 
-//  dba = DBAdaptor_new("kaka.sanger.ac.uk","anonymous",NULL,"homo_sapiens_core_14_31",3306,NULL);
-//  dba = DBAdaptor_new("ecs2b.internal.sanger.ac.uk","ensro",NULL,"human_NCBI33_raw",3306,NULL);
-//  dba = DBAdaptor_new("127.0.0.1","ensro",NULL,"human_NCBI33_raw",13302,NULL);
-//  dba = DBAdaptor_new("127.0.0.1","root",NULL,"test_core",3306,NULL);
-//  dba = DBAdaptor_new("127.0.0.1","root",NULL,"homo_sapiens_core_18_34",3306,NULL);
-//  dba = DBAdaptor_new("127.0.0.1","root",NULL,"steve_feb04_comp",3306,NULL);
-//  dba = DBAdaptor_new("ens-livemirror.internal.sanger.ac.uk","ensadmin","ensembl","homo_sapiens_core_71_37",3306,NULL);
-  dba = DBAdaptor_new("127.0.0.1","ensadmin","ensembl","homo_sapiens_core_72_37",13390,NULL);
-//  dba = DBAdaptor_new("genebuild2.internal.sanger.ac.uk","ensadmin","ensembl","steve_hs_testdb",3306,NULL);
-//  dba = DBAdaptor_new("127.0.0.1","ensadmin","ensembl","steve_hs_testdb",3306,NULL);
+  dba = DBAdaptor_new("ensembldb.ensembl.org","anonymous",NULL,"homo_sapiens_core_86_38",3306,NULL);
 
   return dba;
 }
@@ -45,12 +36,6 @@ Slice *Test_getStandardSlice(DBAdaptor *dba) {
 
   sa = DBAdaptor_getSliceAdaptor(dba);
 
-//  slice = SliceAdaptor_fetchByRegion(sa,"chromosome","2",10000000,100000000,1,NULL,0);
-//  slice = SliceAdaptor_fetchByRegion(sa,"chromosome","1",1000000,260000000,1,NULL,0);
-//  slice = SliceAdaptor_fetchByRegion(sa,"chromosome","2",1000000,260000000,1,NULL,0);
-  //PAR + boundary cross
-  //slice = SliceAdaptor_fetchByRegion(sa,"chromosome","Y",1000000,4000000,1,NULL,0);
-//  slice = SliceAdaptor_fetchByRegion(sa,"chromosome","1",1000000,2000000,1,NULL,0);
   slice = SliceAdaptor_fetchByRegion(sa,"chromosome","20",1000000,260000000,1,NULL,0);
 
   return slice;
